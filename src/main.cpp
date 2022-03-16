61c9e381fd783648a013198df97b17925bc4b6d4
student <student@mini-07.local>
2022-03-16 11:43:47 +0100

    added LaborkaConfig.h.in

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
-int main() { /*...*/ return 0; }
+define _USE_MATH_DEFINES
+#include<cmath>
+#include"LaborkaConfig.h.in"
+int main() {
+ #ifdef USE_TRIGONOMETRY_DEGREE
+double result = cos_degree(45.0);
+ #else
+double pi = 3.141592653589793238462643383279502884;
+double result = cos(pi/4.0);
+ #endif
+ return result;}
